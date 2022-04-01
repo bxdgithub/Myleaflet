4752acf591f4b228d9a8a437d84015232013b31a
bxdgithub <bxd145054@163.com>
2022-04-01 20:03:58 +0800

    4 files modified

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Import stylesheets
 import './style.css';
+// import leaflet
+import { Map, TileLayer } from 'leaflet';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+// create Map
+const map = new Map('map');
+// 高德矢量
+// http://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}
+// 高德影像
+// https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}
+const Layer = new TileLayer(
+  'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
+  { subdomains: '1234' }
+);
+// addmap
+Layer.addTo(map);
+// 设置视图位置与比例
+map.setView([39.90960456049752, 116.3972282409668], 15);
