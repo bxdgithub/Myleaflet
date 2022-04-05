a06765fcc41a8a1ce4a4dd93ad346b9d4f023107
bxdgithub <bxd145054@163.com>
2022-04-05 19:16:32 +0800

    3 files modified

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 // Import stylesheets
 import './style.css';
 // import leaflet
-import { Map, TileLayer } from 'leaflet';
+import { Map, TileLayer, Control } from 'leaflet';
 
 // create Map
 const map = new Map('map');
-// 高德矢量
-// http://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}
-// 高德影像
-// https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}
-const Layer = new TileLayer(
+
+// 高德地图
+const gaodeLayer = new TileLayer(
   'https://webst01.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}',
   { subdomains: '1234' }
 );
+// 腾讯地图
+const tenxunLayer = new TileLayer(
+  'http://rt0.map.gtimg.com/realtimerender?z={z}&x={x}&y={-y}&type=vector&style=0',
+  {}
+);
+// 天地图影像
+// const tiandituLayer = new TileLayer(
+//   'http://t7.tianditu.gov.cn/img_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=img&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=e3b434f191257368fc43c5b011ab5911',
+//   {}
+// );
+// 天地图影像标记
+// const lablLayer = new TileLayer(
+//   'http://t7.tianditu.gov.cn/cia_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cia&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&tk=e3b434f191257368fc43c5b011ab5911',
+//   {}
+// );
 // addmap
-Layer.addTo(map);
+tenxunLayer.addTo(map);
 // 设置视图位置与比例
 map.setView([39.90960456049752, 116.3972282409668], 15);
+const layerContral = new Control.Layers({
+  高德地图: gaodeLayer,
+  腾讯地图: tenxunLayer,
+});
+// addmap
+layerContral.addTo(map);
